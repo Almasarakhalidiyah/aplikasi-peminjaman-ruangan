842ca135a924ca324a59b8ec6f025bb7f5cbfe15
Alma sara khalidiyah <almaclaudia43@gmail.com>
2023-12-27 18:26:27 +0700

    tambahkan file

---
+++ app/src/main/java/com/example/selfhealth_app/activity/DetailOrderActivity.kt
@@ -0,0 +1,13 @@
+package com.example.selfhealth_app.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.selfhealth_app.R
+
+class DetailOrderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_order)
+    }
+
+}
---
+++ app/src/main/java/com/example/selfhealth_app/activity/LoginActivity.kt
@@ -0,0 +1,52 @@
+package com.example.selfhealth_app.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.selfhealth_app.R
+import com.example.selfhealth_app.helper.Constant
+import com.example.selfhealth_app.helper.PreferencesHelper
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+
+    lateinit var  sharedpref : PreferencesHelper
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        sharedpref = PreferencesHelper(this)
+
+        buttonLogin.setOnClickListener {
+            if (editEmail.text.isNotEmpty() && editPassword.text.isNotEmpty()) {
+                saveSession(editEmail.text.toString(), editPassword.text.toString())
+                showMessage("Login Success")
+                moveIntent()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (sharedpref.getBoolean(Constant.PREF_IS_LOGIN)) {
+            moveIntent()
+        }
+    }
+
+    private fun moveIntent() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    private fun saveSession(email: String, password: String) {
+        sharedpref.put(Constant.PREF_EMAIL, email)
+        sharedpref.put(Constant.PREF_PASSWORD, password)
+        sharedpref.put(Constant.PREF_IS_LOGIN, true)
+    }
+
+    private fun showMessage(message: String) {
+        Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+    }
+}
